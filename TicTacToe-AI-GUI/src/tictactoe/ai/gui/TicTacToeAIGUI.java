32881af5111237ccd6aa62e42684396e6ea09233
Krishna Jalan <krishnajalan2001@gmail.com>
2020-10-21 15:52:07 +0530

    Update TicTacToeAIGUI.java

--- TicTacToe-AI-GUI/src/tictactoe/ai/gui/TicTacToeAIGUI.java
+++ TicTacToe-AI-GUI/src/tictactoe/ai/gui/TicTacToeAIGUI.java
@@ -16,6 +16,7 @@ public class TicTacToeAIGUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new Menu().setVisible(true);
     }
     
 }
